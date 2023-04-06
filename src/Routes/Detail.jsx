ed160a556472ee63c239151ca41b800046da3079
yessenia <yejiga2006@gmail.com>
2023-04-06 18:25:35 -0500

    funcional, falta estilo de detail

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useGlobalSates } from "../Components/utils/Context";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -7,6 +8,7 @@ const Detail = () => {
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
   const { id } = useParams();
   const [odontologo, setOdontolgo] = useState([]);
+  const { state, dispatch } = useGlobalSates();
 
   const url = `https://jsonplaceholder.typicode.com/users/${id}`;
   useEffect(() => {
@@ -19,7 +21,7 @@ const Detail = () => {
     fetchData();
   }, []);
   return (
-    <>
+    <div className={` ${state.theme}`}>
       <h1>Detail Dentist {id} </h1>
 
       <p>name={odontologo.name}</p>
@@ -29,7 +31,7 @@ const Detail = () => {
 
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
-    </>
+    </div>
   );
 };
 
